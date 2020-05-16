9fccc7467f043582bf9d662e3b676434ee12e226
Jordan Dykstra <jtdykstra123@gmail.com>
2020-05-16 16:59:46 -0700

    Commit Static Files

---
+++ todo_client/build/precache-manifest.8e968480b22e0e35a0db5dc0653b521c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8b721942072af1595bc1ee89629394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fad0ddcb020b639a5b64",
+    "url": "/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "810fc9f6205e4b7be624",
+    "url": "/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "fad0ddcb020b639a5b64",
+    "url": "/static/js/2.3781f970.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.3781f970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810fc9f6205e4b7be624",
+    "url": "/static/js/main.e7564d1b.chunk.js"
+  },
+  {
+    "revision": "fd205a47de5f1c15a867",
+    "url": "/static/js/runtime-main.f2fa0a46.js"
+  }
+]);
